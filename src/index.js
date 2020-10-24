561ba7cfeaf994f92dcc0f4e94f5db62c1dd404b
Anton <kubana6@gmail.com>
2020-10-24 23:48:04 +0300

    finish: make task brakets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = false;
+  let stack = [];
+  let openBrackets = [];
+  let closeBrackets = [];
+  let isOpenBracket;
+  const strElements = str.split("");
+  const strElementsLength = strElements.length;
+
+  bracketsConfig.forEach(element => {
+    openBrackets.push(element[0]);
+    closeBrackets.push(element[1]);
+  });
+  const openBracketsLength = openBrackets.length;
+
+  for (let i = 0; i < strElementsLength; i++) {
+    isOpenBracket = false;
+
+    if (stack.length > 0) {
+      lastStackElement = stack[stack.length - 1];
+      if (strElements[i] === closeBrackets[lastStackElement]) {
+        stack.pop();
+        result = true;
+        continue;
+      }
+    }
+
+    for (let j = 0; j < openBracketsLength; j++) {
+      if (strElements[i] === openBrackets[j]) {
+        stack.push([j]);
+        result = false;
+        isOpenBracket = true;
+        break;
+      }
+    }
+    if (isOpenBracket) continue;
+
+    result = false;
+    break;
+  }
+  return result;
 }
